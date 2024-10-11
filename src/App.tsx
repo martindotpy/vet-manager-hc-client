4eb916a7d069225b37dc59908760e3a4c4bfb379
ZoombieAlc <zoomiealc@gmail.com>
2024-10-11 11:45:24 -0500

    tailwindcss working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <div className="w-screen h-screen flex justify-center items-center">
+    <div className="bg-white h-max w-auto rounded-xl p-5 flex flex-col items-center">
+      <img src="public/electron-vite.animate.svg" className="w-36" alt="" />
+      <div>
+        <p className="text-lg text-black ">Usuario</p>
+      <input type="text" className="border-2 border-gray-300 text-black rounded-lg p-2 bg-white" />
+      </div>
+      
+      <div>
+      <p className="text-lg text-black ">Contraseña</p>
+      <input type="password" className="border-2 border-gray-300 text-black rounded-lg p-2 bg-white" />
+      </div>
+      <button className="bg-accent hover:bg-secondary hover:text-black transition ease-in-out delay-150 text-white rounded-lg p-2 mt-5 w-full">Iniciar Sesión</button>
+    </div>
+  </div>
+  </>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#66B4C1',
+        'secondary':'#94B9C7',
+        'accent': '#2A5594'
+      }
+    },
   },
   plugins: [],
 };
